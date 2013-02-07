a523ce5a35909f7558e1dd12a862efe42dd492e1
Lucas <visiusback@yahoo.com.ar>
2013-02-06 22:39:32 -0300

    probando segundo push

--- Proyectos/MyFirstApp/src/com/example/myfirstapp/MainActivity.java
+++ Proyectos/MyFirstApp/src/com/example/myfirstapp/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+        //comentario para probar push
     }
     
 }
